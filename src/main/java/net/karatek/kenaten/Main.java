f7394a2ab2ea63a8b0093042e874e83a1619c547
KaratekHD <kontakt@karatek.net>
2020-03-05 11:20:41 +0100

    Changed version to 0.2 pre-alpha

--- src/main/java/net/karatek/kenaten/Main.java
+++ src/main/java/net/karatek/kenaten/Main.java
@@ -40,7 +40,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static final String VERSION = "0.1 pre-alpha";
+    public static final String VERSION = "0.2 pre-alpha";
 
     // Initialize Logger
     public static final Logger logger = LogManager.getLogger(Main.class);
